4c61d3f6a9862ce7919911b3722bb2816314e128
Bill bilguun <bilguun.ts@infinite.mn>
2022-04-21 18:20:18 +0800

    h

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -10,26 +10,36 @@ c.moveTo(0,canvas.height/2);
 
 
 
-const wave = {
+let wave = {
     y:canvas.height/2,
     length:0.01,
     amplitude:50,
     frequency :0.01,
+    color:0,
 }
 gui.add(wave,'y',0,canvas.height);
 gui.add (wave, 'amplitude',-400, 400);
 gui.add(wave, 'length',-0.001,0.05)
-gui.add(wave, 'frequency',-0.01,1);
-
+gui.add(wave, 'frequency',-0.01,5);
+//gui.add(wave, 'color', 0,255);
 let increment =wave.frequency;
 function SinWave(){
     c.beginPath();
-    c.clearRect(0,0,canvas.width,canvas.height);
+    c.fillStyle='rgba(0, 0, 0, 0.2)';
+   // c.clearRect(0,0,canvas.width,canvas.height);
     for (let i  =0 ; i <canvas.width; i ++){
-        c.lineTo(i,wave.y+ wave.amplitude* Math.sin(i*wave.length+increment))
+        c.lineTo(i,wave.y+ wave.amplitude*Math.cos(increment)* Math.sin(i*wave.length+increment))
     }
-    increment  += wave.frequency;
+    c.strokeStyle=`hsl(${wave.color} , 50% , 50% )`
+
     c.stroke();
+    increment  += wave.frequency;    
+    if(wave.color>=255){
+        wave.color = wave.color-1;
+    }else if(wave.color<=0){
+        wave.color= wave.color+1;
+    };
+
 }
 function update (){
     SinWave();
